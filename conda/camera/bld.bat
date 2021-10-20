12e6116a547d9f36cefabfb73e1eb28a4db38365
Samuel Debionne <samuel.debionne@esrf.fr>
2021-10-20 11:00:15 +0200

    [CI][CONDA] Switch to VS2019, Ninja, remove old Python builds and dependency to numpy

--- conda/camera/bld.bat
+++ conda/camera/bld.bat
@@ -1,14 +1,14 @@
 echo "... [camera/bld.bat] enter"
 
-python get_git_log.py 
+REM python get_git_log.py 
 
 echo "... [camera/bld.bat] cmake compile"
-cmake -Bbuild -H. -G "%CMAKE_GENERATOR%" -DLIMA_ENABLE_PYTHON=1 -DWITH_GIT_VERSION=1 -DLIMA_ENABLE_DEBUG=1 -DCAMERA_ENABLE_TESTS=1 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPYTHON_SITE_PACKAGES_DIR=%SP_DIR% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
+cmake -Bbuild -H. -G "Ninja" -DLIMA_ENABLE_PYTHON=1 -DWITH_GIT_VERSION=1 -DLIMA_ENABLE_DEBUG=1 -DCAMERA_ENABLE_TESTS=1 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPYTHON_SITE_PACKAGES_DIR=%SP_DIR% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 
 echo "... [camera/bld.bat] cmake build"
-cmake --build build --config Release --target install -- /verbosity:minimal
+cmake --build build --config Release --target install
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 echo "... [camera/bld.bat] exit"
