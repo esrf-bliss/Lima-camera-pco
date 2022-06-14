7990ae6f33ca7fbe676f2789dfdacbafd3872319
Samuel Debionne <samuel.debionne@esrf.fr>
2022-06-14 14:38:04 +0200

    [CONDA] Update SIP dependency and build for Python 3.7, 3.8 and 3.9

--- conda/tango/bld.bat
+++ conda/tango/bld.bat
@@ -1,12 +1,7 @@
-echo "... [tango/bld.bat] enter"
 cd tango
 
-echo "... [tango/bld.bat] cmake compile"
-cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPYTHON_SITE_PACKAGES_DIR=%SP_DIR% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
+cmake -Bbuild -H. -G "Ninja" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-echo "... [tango/bld.bat] cmake build"
-cmake --build build --config Release --target install
+cmake --build build --target install
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-echo "... [tango/bld.bat] exit"
